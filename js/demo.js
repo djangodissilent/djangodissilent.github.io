098cdb574b84f16aa20e9b7cd7ec7e2f64977670
Rslan <mrdjangodev@gmail.com>
2021-08-09 15:35:30 +0200

    edited readme

--- js/demo.js
+++ js/demo.js
@@ -1,12 +1,14 @@
 
 document.addEventListener('DOMContentLoaded', function () {
   particleground(document.getElementById('particles'), {
-    dotColor: '#5cbdaa',
-    lineColor: '#5cbdaa',
-    density:10000,
+    dotColor: '#0392cf',
+    lineColor: '#ff0938',
+    density:10000 - 1000,
     // proximity:105,
     // particleRadius:2,
     // Dotsize:15,
+    parallax:0,
+    
   });
   var intro = document.getElementById('intro');
   intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
